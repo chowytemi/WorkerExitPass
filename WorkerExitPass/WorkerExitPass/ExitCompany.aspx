032c0a06d2068cfc39b6db063b815f6495ef9354
Jihan Shafitri YUSOFF <T203@dyna-mac.com>
2022-11-16 15:58:50 +0800

    create exit company page

--- WorkerExitPass/WorkerExitPass/ExitCompany.aspx
+++ WorkerExitPass/WorkerExitPass/ExitCompany.aspx
@@ -4,11 +4,29 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Exit Company</title>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@700&display=swap" rel="stylesheet" />
+    <link href="Content/StyleSheet5.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
+            <h1>Exit Company</h1>
+
+            <div>
+                 <asp:Label class="label" ID="label" runat="server" Text="Employee ID"></asp:Label>
+                 <input class="input" id="lblEmpID" type="text"/>
+
+                 <asp:Label class="label" ID="label1" runat="server" Text="Name of Company"></asp:Label>
+                 <asp:DropDownList class="dropdown" ID="ddlCompany" runat="server" AutoPostBack="true">
+                 </asp:DropDownList>
+
+                 <asp:Button class="button" ID="submitBtn" runat="server" Text="Submit" />
+
+            </div>
         </div>
     </form>
 </body>
--- WorkerExitPass/WorkerExitPass/ExitCompany.aspx.cs
+++ WorkerExitPass/WorkerExitPass/ExitCompany.aspx.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+
 
 namespace WorkerExitPass
 {
@@ -11,7 +18,23 @@ namespace WorkerExitPass
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            BindDataSetDataCompany();
+        }
 
+        protected void BindDataSetDataCompany()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["appusers"].ConnectionString;
+            SqlConnection con = new SqlConnection(cs);
+            SqlDataAdapter da = new SqlDataAdapter("select distinct company from EmpList", con);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            ds.Tables[0].Columns.Add("Company", typeof(string), "company");
+
+            ddlCompany.DataTextField = "Company";
+            ddlCompany.DataValueField = "company";
+            ddlCompany.DataSource = ds;
+            ddlCompany.DataBind();
+            ddlCompany.Items.Insert(0, new ListItem("Select Company", "0"));
         }
     }
 }
